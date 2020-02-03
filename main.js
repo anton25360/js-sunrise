032684707e009492c5c4d355cd328687ded7d994
Anton Webb <anton25360@gmail.com>
2020-02-03 16:08:55 +0000

    added dummy fetch request

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 "use strict";
 console.log('hi there my dude');
+fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    console.log(data);
+});
+fetch('https://randomuser.me/api/')
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    console.log(data.results[0].location.coordinates.latitude);
+    console.log(data.results[0].location.coordinates.longitude);
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
 console.log('hi there my dude');
+
+fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
+    .then((response) => response.json())
+    .then((data) => {
+
+        console.log(data);
+
+    })
+
+
+
+fetch('https://randomuser.me/api/')
+    .then((response) => response.json())
+    .then((data) => {
+
+        console.log(data.results[0].location.coordinates.latitude);
+        console.log(data.results[0].location.coordinates.longitude);
+
+    })
